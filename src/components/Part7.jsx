2d6b41127743ac4aea8fac7d59dd93e4b5e4d901
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-04-20 11:28:25 +0800

    feat: add Part7(sm, md, ipad, lg version)

--- src/components/Part7.jsx
+++ src/components/Part7.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { device } from '../globalStyles';
 
 import Button from './elements/Button';
 
@@ -8,18 +9,12 @@ import star from '../assets/star.png';
 const StyledDiv = styled.div`
   position: relative;
   min-height: 240px;
+  height: 60vw;
 
   .background {
     min-height: 240px;
+    height: 100%;
     object-fit: cover;
-  }
-
-  .star {
-    position: absolute;
-    z-index: 2;
-    top: 49px;
-    left: 50%;
-    transform: translateX(-50%);
   }
 
   .content {
@@ -27,10 +22,10 @@ const StyledDiv = styled.div`
     bottom: 0;
 
     width: 100%;
-    height: 170px;
+    height: 75%;
     background-color: #fff;
     border-top: 1px solid var(--pink-2);
-    border-radius: 160px 160px 0 0;
+    border-radius: 99999px 99999px 0 0;
     padding-bottom: 28px;
 
     display: flex;
@@ -48,14 +43,67 @@ const StyledDiv = styled.div`
       font-size: 10px;
     }
   }
+
+  .star {
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  @media screen and (${device.sm}) {
+    .content {
+      padding-bottom: 40px;
+
+      h3 {
+        font-size: 1.5rem;
+      }
+
+      p {
+        margin: 17px;
+        font-size: 1.25rem;
+      }
+    }
+  }
+
+  @media screen and (${device.md}) {
+    .content {
+      padding-bottom: 50px;
+
+      h3 {
+        font-size: 2.25rem;
+      }
+
+      p {
+        font-size: 1.75rem;
+        margin-bottom: 40px;
+      }
+    }
+  }
+
+  @media screen and (${device.ipad}) {
+    .content {
+      height: 80%;
+    }
+  }
+
+  @media screen and (${device.lg}) {
+    height: auto;
+
+    .content {
+      height: 90%;
+    }
+  }
 `;
 
 function Part7() {
   return (
     <StyledDiv>
       <img src={background4} alt="background" className="background" />
-      <img src={star} alt="星星" className="star" />
+
       <div className="content">
+        <img src={star} alt="星星" className="star" />
         <h3>你要的修煉都在 The F2E</h3>
         <p>三週精實修煉，一次提升實力</p>
         <Button>我要報名</Button>
