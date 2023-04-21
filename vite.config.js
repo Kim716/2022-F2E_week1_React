88dc74a6b77b945b95681c29f6b03325192eb5e2
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-04-21 19:54:57 +0800

    feat: add deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/2022-F2E_week1_React/',
   /*
   optimizeDeps: {
     exclude: ['sweetalert2'],
